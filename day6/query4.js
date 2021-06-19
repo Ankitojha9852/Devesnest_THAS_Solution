9c545ce25108d1027fce24daac0b1a0d3d504a12
Ankitojha9852 <goluankit.ojha@gmail.com>
2021-06-19 17:40:41 +0530

    day_six_work

---
+++ day6/query4.js
@@ -0,0 +1,8 @@
+
+function joiner(params) {
+  const first = params.join(",");
+  const second = params.join("+");
+  return { first, second };
+}
+
+console.log(joiner(["Red", "Green", "White", "Black"]));
---
+++ day6/query5.js
@@ -0,0 +1,22 @@
+function freq(params) {
+  const newMap = new Map();
+  for (let i = 0; i < params.length; i++) {
+    if (newMap.has(params[i])) {
+      newMap.set(params[i], newMap.get(params[i]) + 1);
+    } else {
+      newMap.set(params[i], 1);
+    }
+  }
+  let maxValue = 0;
+  let maxKey = 0;
+  for (let [key, value] of newMap) {
+    if (value > maxValue) {
+      maxValue = value;
+      maxKey = key;
+    }
+  }
+  return { maxKey, maxValue };
+}
+
+const x = freq([3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3]);
+console.log(`${x.maxKey} ( ${x.maxValue} times )`);
